512f218a4641d5764becdfaaec7f8a09920d3531
wmkania <wmkania@gmail.com>
2021-07-25 14:34:27 +0100

    Added all files

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+function newItem(){
+
+let listItem = $('<li><li>');
+let inputValue = $('#input').val();
+listItem.append(inputValue);
+
+   if (inputValue === '') {
+     alert("You must write something!");
+   } else {
+     $('#list').append(listItem);
+   }
+
+   function crossOut() {
+ 		listItem.toggleClass("strike");
+ 	}
+
+listItem.on("dblclick", function crossOut() {
+  listItem.toggleClass("strike");
+});
+
+
+let crossOutButton= $('<crossOutButton></crossOutButton>');
+ crossOutButton.append(document.createTextNode('X'));
+ listItem.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
+  function deleteListItem(){
+    listItem.addClass("delete")
+  }
+
+   $('#list').sortable();
+
+}
