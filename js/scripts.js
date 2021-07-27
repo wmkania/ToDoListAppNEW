484dd9c9032d34c1caa21affc297533c331299b7
wmkania <wmkania@gmail.com>
2021-07-27 11:45:11 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    li.append(inputValue);
 
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      $('#list').append(li);
-    }
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-    function crossOut() {
-  		li.toggleClass("strike");
-  	}
-
-  	li.on("dblclick", function crossOut() {
-  		li.toggleClass("strike");
-  	});
-
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
-
-  crossOutButton.on("click", deleteListItem);
-  function deleteListItem(){
-  li.addClass("delete")
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
   }
-     $('#list').sortable();
 
+  function crossOut() {
+		li.toggleClass("strike");
+	}
 
+	li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
+function deleteListItem(){
+li.addClass("delete")
 }
+
+   $('#list').sortable();
